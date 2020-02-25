1b6194ad542395ca39e520fc7dbcf5ad606bf852
SumanSudhir <sudhiriitb27@gmail.com>
2020-02-25 11:47:36 +0530

    error resolved

--- recommendation/NeuMF.swift
+++ recommendation/NeuMF.swift
@@ -1,13 +1,26 @@
 import TensorFlow
 import Foundation
 
-public struct NeuMF: Layer {
+public struct NeuMF: Module {
+
+    public typealias Scalar = Float
+
     @noDerivative public let num_users: Int
     @noDerivative public let num_items: Int
     @noDerivative public let mf_dim: Int
-    @noDerivative public let mf_reg: Float = 0.0
-    @noDerivative public let mlp_layer_sizes : [Int] = [64,32,16,8]
-    @noDerivative public let mlp_layer_regs: [Float] = [0,0,0,0]
+    @noDerivative public let mf_reg: Scalar
+    @noDerivative public var mlp_layer_sizes : [Int] = [64,32,16,8]
+    @noDerivative public var mlp_layer_regs: [Scalar] = [0,0,0,0]
+
+    public var mf_user_embed: Embedding<Scalar>
+    public var mf_item_embed: Embedding<Scalar>
+    public var mlp_user_embed: Embedding<Scalar>
+    public var mlp_item_embed: Embedding<Scalar>
+    public var dense1: Dense<Scalar>
+    public var dense2: Dense<Scalar>
+    public var dense3: Dense<Scalar>
+    public var final_dense: Dense<Scalar>
+
 
     public init(
         num_users: Int,
@@ -23,46 +36,49 @@ public struct NeuMF: Layer {
         self.mf_reg = mf_reg
         self.mlp_layer_sizes = mlp_layer_sizes
         self.mlp_layer_regs = mlp_layer_regs
-        let num_mlp_layers = self.mlp_layer_sizes.count
 
-        precondition(self.mlp_layer_sizes[0]%2 == 0, "u dummy, mlp_layer_sizes[0] % 2 != 0")
-        precondition(self.mlp_layer_sizes.count == self.mlp_layer_regs.count, "u dummy, layer_sizes != layer_regs!")
+        // precondition(self.mlp_layer_sizes[0]%2 == 0, "u dummy, mlp_layer_sizes[0] % 2 != 0")
+        // precondition(self.mlp_layer_sizes.count == self.mlp_layer_regs.count, "u dummy, layer_sizes != layer_regs!")
 
         //TODO: regularization
-        var self.mf_user_embed = Embedding<Float>(vocabularySize: self.num_users, embeddingSize: self.mf_dim)
-        var self.mf_item_embed = Embedding<Float>(vocabularySize: self.num_items, embeddingSize: self.mf_dim)
-        var self.mlp_user_embed = Embedding<Float>(vocabularySize: self.num_users, embeddingSize: floor(self.mf_layer_sizes[0]/2))
-        var self.mlp_item_embed = Embedding<Float>(vocabularySize: self.num_items, embeddingSize: floor(self.mf_layer_sizes[0]/2))
+        self.mf_user_embed = Embedding<Scalar>(vocabularySize: self.num_users, embeddingSize: self.mf_dim)
+        self.mf_item_embed = Embedding<Scalar>(vocabularySize: self.num_items, embeddingSize: self.mf_dim)
+        self.mlp_user_embed = Embedding<Scalar>(vocabularySize: self.num_users, embeddingSize: self.mlp_layer_sizes[0]/2)
+        self.mlp_item_embed = Embedding<Scalar>(vocabularySize: self.num_items, embeddingSize: self.mlp_layer_sizes[0]/2)
 
         //TODO: Extend it for n layers by using for loop
         //Currently only for 3 layers
-        var dense1 = Dense(inputSize: self.mlp_layer_sizes[0], outputSize: self.mlp_layer_sizes[1], activation: relu)
-        var dense2 = Dense(inputSize: self.mlp_layer_sizes[1], outputSize: self.mlp_layer_sizes[2], activation: relu)
-        var dense3 = Dense(inputSize: self.mlp_layer_sizes[2], outputSize: self.mlp_layer_sizes[3], activation: relu)
-        var final_dense = Dense(inputSize: (self.mlp_layer_sizes[3] + self.mf_dim), outputSize: 1, activation: sigmoid)
-
+        dense1 = Dense(inputSize: self.mlp_layer_sizes[0], outputSize: self.mlp_layer_sizes[1], activation: relu)
+        dense2 = Dense(inputSize: self.mlp_layer_sizes[1], outputSize: self.mlp_layer_sizes[2], activation: relu)
+        dense3 = Dense(inputSize: self.mlp_layer_sizes[2], outputSize: self.mlp_layer_sizes[3], activation: relu)
+        final_dense = Dense(inputSize: (self.mlp_layer_sizes[3] + self.mf_dim), outputSize: 1, activation: sigmoid)
+    }
         @differentiable
-        public func callAsFunction(_ user_indices: Int , _ item_indices: Int) -> Tensor<Float>{
-            var user_embed_mlp = self.mlp_user_embed(user_indices)
-            var item_embed_mlp = self.mlp_item_embed(item_indices)
-            var user_embed_mf = self.mf_user_embed(user_indices)
-            var item_embed_mf = self.mf_item_embed(item_indices)
+        public func callAsFunction(_ input: Tensor<Int32>) -> Tensor<Scalar>{
+            let user_indices  = input[0]
+            let item_indices = input[1]
 
-            var mf_vector = matmul(user_embed_mf,item_embed_mf)
+            let user_embed_mlp = self.mlp_user_embed(user_indices)
+            let item_embed_mlp = self.mlp_item_embed(item_indices)
+            let user_embed_mf = self.mf_user_embed(user_indices)
+            let item_embed_mf = self.mf_item_embed(item_indices)
+
+            let mf_vector = matmul(user_embed_mf,item_embed_mf)
             var mlp_vector = user_embed_mlp.concatenated(with:item_embed_mlp,alongAxis:-1)
 
             mlp_vector = mlp_vector.sequenced(through: dense1, dense2, dense3)
-            var vector = mlp_vector.concatenated(with:mf_vector,alongAxis:-1)
+            let vector = mlp_vector.concatenated(with:mf_vector,alongAxis:-1)
 
-            return vector.sequenced(through: final_dense)
+            return final_dense(vector)
+            // return mlp_vector
         }
-    }
+    // }
 }
 
 
-// var size = [64, 32, 16, 8]
-// var regs = [0, 0, 0, 0]
-// var model = NeuMf(num_users: 5, num_items: 3, mf_dim: 10, mf_reg: 0, mlp_layer_sizes: size, mlp_layer_regs: regs)
+// var size:[Int] = [64, 32, 16, 8]
+// var regs:[Float] = [0, 0, 0, 0]
+// var model = NeuMF(num_users: 5, num_items: 3, mf_dim: 10, mf_reg: 0.0, mlp_layer_sizes: size, mlp_layer_regs: regs)
 //
 // let optimizer = SGD(for: model, learningRate: 0.001)
 //
